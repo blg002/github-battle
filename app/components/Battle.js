062d66e280a15e934679e57287f3731794eac4f6
Brad Graham <blg002@gmail.com>
2018-07-03 16:13:00 -0400

    routing

---
+++ app/components/Battle.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var Link = require('react-router-dom').Link;
+
+
+class Battle extends React.Component {
+	render() {
+		return (
+			<div>
+				BATTLE1
+			</div>
+		)
+	}
+}
+
+
+module.exports = Battle;
