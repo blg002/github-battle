67147411142841738417da63c986d458beb0dded
Brad Graham <blg002@gmail.com>
2018-07-21 23:33:56 -0400

    Building a Highly Reusable Component

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,6 +4,7 @@ var queryString = require('query-string');
 var api = require('../utils/api')
 var PlayerPreview = require('./PlayerPreview');
 var Link = require('react-router-dom').Link;
+var Loading = require('./Loading')
 
 
 function Profile (props) {
@@ -89,7 +90,7 @@ class Results extends React.Component {
 		var loading = this.state.loading;
 
 		if (loading === true) {
-			return <p>Loading</p>
+			return <Loading text='Downloading' />
 		}
 
 		if (error) {
