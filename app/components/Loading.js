ef284e404969d5f701d9c70ccc7670a1c5c90b88
Brad Graham <blg002@gmail.com>
2018-07-21 23:47:19 -0400

    custom speed

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -19,7 +19,7 @@ class Loading extends React.Component {
 	}
 
 	componentDidMount() {
-		var stopper = this.props.text = '...';
+		var stopper = this.props.text + '...';
 		this.interval = window.setInterval(function () {
 			if (this.state.text === stopper) {
 				this.setState(function () {
@@ -34,8 +34,12 @@ class Loading extends React.Component {
 					}
 				});
 			}
-		}.bind(this), 300)
+		}.bind(this), this.props.speed)
 	}
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval)
+  }
 
 	render() {
 		return (
@@ -48,12 +52,13 @@ class Loading extends React.Component {
 
 
 Loading.propTypes = {
-	text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+	speed: PropTypes.number.isRequired,
 }
 
-
 Loading.defaultProps = {
-	text: 'Loading'
+  text: 'Loading',
+	speed: 300,
 }
 
 module.exports = Loading;
